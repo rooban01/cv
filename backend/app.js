84c64bc20151017a9c6c898fa9bcb7c7d7dcf787
rooban01 <47852102+rooban01@users.noreply.github.com>
2020-11-19 13:34:32 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const cvRoutes = require("./routes/cv");
 const app = express();
 
 
-mongoose.connect("mongodb+srv://+"@cluster0-vbpzi.mongodb.net/cv-contact") // ?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://+"@cgodb.net/cv-contact") // ?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log("Connected to database!");
   })
